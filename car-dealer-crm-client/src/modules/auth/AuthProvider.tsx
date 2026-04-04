db0386f2ec0a48f715794347aa4519e82a54191e
Ruslan Filyk <filyk31@gmail.com>
2026-04-04 20:24:44 +0300

    remove google auth methods

--- car-dealer-crm-client/src/modules/auth/AuthProvider.tsx
+++ car-dealer-crm-client/src/modules/auth/AuthProvider.tsx
@@ -1,13 +1,11 @@
 import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
 import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, updateProfile, type Auth, type User, type UserCredential } from "firebase/auth";
 import { auth } from "./firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 type TAuthContext = {
     auth: Auth;
     user: User | null;
     login: (email: string, password: string) => Promise<UserCredential>;
-    loginWithGoogle: () => Promise<UserCredential>;
     register: (email: string, password: string, name: string) => Promise<UserCredential>;
     logout: () => Promise<void>;
 }
@@ -23,16 +21,7 @@ export function AuthProvider({ children }: PropsWithChildren) {
     return signInWithEmailAndPassword(auth, email, password);
   }
 
-    // Inside your AuthProvider function:
-    function loginWithGoogle() {
-    const provider = new GoogleAuthProvider();
-    // Optional: Force account selection if they have multiple Google accounts
-    provider.setCustomParameters({ prompt: 'select_account' });
-
-    return signInWithPopup(auth, provider);
-    }
-
-  async function register(email: string, password: string, name: string): Promise<UserCredential> {
+async function register(email: string, password: string, name: string): Promise<UserCredential> {
     const credential = await createUserWithEmailAndPassword(auth, email, password);
     await updateProfile(credential.user, { displayName: name });
     return credential;
@@ -52,7 +41,7 @@ export function AuthProvider({ children }: PropsWithChildren) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ auth, user, login, loginWithGoogle, register, logout }}>
+    <AuthContext.Provider value={{ auth, user, login, register, logout }}>
       {!loading && children}
     </AuthContext.Provider>
   );
